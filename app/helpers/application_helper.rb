ff46de5aa882aece294d8e0054117839d3027db7
dhartiodedara27 <dharti.odedara@aspireedge.com>
2025-12-31 18:40:01 +0530

    REFINE: Implement dynamic breadcrumbs and page title in navbar, add search header partial for user index view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  # breadcrumbs
+  def dynamic_breadcrumbs
+    crumbs = [{ label: "Home", path: root_path }]
+    
+    controller_label = controller_name.titleize
+    index_path = url_for(controller: controller_path, action: :index) rescue nil
+
+    crumbs << { label: controller_label, path: index_path }
+
+    unless action_name == "index"
+      crumbs << { label: action_name.titleize, path: nil }
+    end
+
+    crumbs
+  end
+
+  def dynamic_page_title
+    action_name == "index" ? controller_name.titleize : action_name.titleize
+  end
 end
