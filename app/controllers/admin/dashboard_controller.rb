6a8b6109a21be106205f81efd44bf3609dd3e801
dhartiodedara27 <dharti.odedara@aspireedge.com>
2026-01-06 19:10:32 +0530

    ADD: show counts on dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,20 @@
 # app/controllers/admin/dashboard_controller.rb
 module Admin
   class DashboardController < ApplicationController
-
-      before_action :authenticate_admin_user!
+    before_action :authenticate_admin_user!
 
     def index
+      @stores = Store.count
+      @customers_count = Customer.count
+      @orders_count = Order.count
+      @workers_count = Worker.count
+      @job_roles_count = JobRole.count
+      @garment_types = GarmentType.count
+      @currencies = Currency.count
+      @stores_limit_reached_count =
+  Store.includes(:orders, :user).count(&:order_limit_reached?)
+
     end
+
   end
 end
