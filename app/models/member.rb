65f172eb7770c25be92d74b0a32172a406da4886
dhartiodedara27 <dharti.odedara@aspireedge.com>
2026-01-07 12:28:01 +0530

    FIXES :  add missing associations causing issue in destroying object

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,7 +3,7 @@ class Member < ApplicationRecord
   belongs_to :customer
   has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
-
+  has_many :order_measurements, dependent: :destroy
   # Validations
   validates :name, presence: true
 end
