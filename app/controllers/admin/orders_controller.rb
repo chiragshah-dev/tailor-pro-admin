d4597409cefed408654e0dcb686202c416953c32
aspireparth <parth.patel@aspireedge.com>
2026-02-10 18:32:35 +0530

    Add sortable columns to admin orders index

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,10 +4,38 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin_user!
   before_action :set_order, only: [:show, :history]
 
+  # def index
+  #   @orders = Order.left_joins(:customer, :store)
+  #     .includes(:customer, :store)
+  #     .order(created_at: :desc)
+
+  #   if params[:status].present?
+  #     @orders = @orders.where(status: params[:status])
+  #   end
+
+  #   if params[:search].present?
+  #     search = "%#{params[:search].strip}%"
+  #     @orders = @orders.where(
+  #       "
+  #     orders.order_number ILIKE :search
+  #     OR customers.name ILIKE :search
+  #     OR customers.contact_number ILIKE :search
+  #     OR stores.name ILIKE :search
+  #     OR CAST(stores.code AS TEXT) ILIKE :search
+  #   ",
+  #       search: search,
+  #     )
+  #   end
+
+  #   @orders = @orders.page(params[:page]).per(10)
+  # end
+
   def index
-    @orders = Order.left_joins(:customer, :store)
-      .includes(:customer, :store)
-      .order(created_at: :desc)
+    params.permit(:search, :page, :sort, :direction, :status)
+
+    @orders = Order
+                .left_joins(:customer, :store)
+                .includes(:customer, :store)
 
     if params[:status].present?
       @orders = @orders.where(status: params[:status])
@@ -17,17 +45,35 @@ class Admin::OrdersController < ApplicationController
       search = "%#{params[:search].strip}%"
       @orders = @orders.where(
         "
-      orders.order_number ILIKE :search
-      OR customers.name ILIKE :search
-      OR customers.contact_number ILIKE :search
-      OR stores.name ILIKE :search
-      OR CAST(stores.code AS TEXT) ILIKE :search
-    ",
-        search: search,
+          orders.order_number ILIKE :search
+          OR customers.name ILIKE :search
+          OR customers.contact_number ILIKE :search
+          OR stores.name ILIKE :search
+          OR CAST(stores.code AS TEXT) ILIKE :search
+        ",
+        search: search
       )
     end
 
-    @orders = @orders.page(params[:page]).per(10)
+    sortable_columns = {
+      "order_number" => "orders.order_number",
+      "customer"     => "customers.name",
+      "store"        => "stores.name",
+      "status"       => "orders.status",
+      "total"        => "orders.total_bill_amount"
+    }
+
+    sort_column =
+      sortable_columns[params[:sort]] || "orders.created_at"
+
+    sort_direction =
+      params[:direction] == "asc" ? "asc" : "desc"
+
+    @orders = @orders
+                .references(:customer, :store)
+                .order("#{sort_column} #{sort_direction}")
+                .page(params[:page])
+                .per(10)
   end
 
   def show
