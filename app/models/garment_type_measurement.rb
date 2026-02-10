df4f72f3cc1fab442b082ce20b6fc4167871b465
aspireparth <parth.patel@aspireedge.com>
2026-02-10 11:56:33 +0530

    gt fixes for sequence and ui fixes

--- app/models/garment_type_measurement.rb
+++ app/models/garment_type_measurement.rb
@@ -5,9 +5,9 @@ class GarmentTypeMeasurement < ApplicationRecord
   belongs_to :measurement_field
 
   validates :sequence,
-              numericality: { only_integer: true },
-              uniqueness: { scope: :garment_type_id },
-              allow_nil: true
+          presence: true,
+          numericality: { only_integer: true },
+          uniqueness: { scope: :garment_type_id }
 
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "garment_type_id", "id", "measurement_field_id", "updated_at"]
