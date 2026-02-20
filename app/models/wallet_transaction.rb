a0f837e7126d6673df036f617c2fb9e32461b203
Nisha <root@developer-HP-EliteBook-840-G3.(none)>
2026-02-20 15:58:53 +0530

    Fix enum values for transaction_type in WalletTransaction model

--- app/models/wallet_transaction.rb
+++ app/models/wallet_transaction.rb
@@ -4,6 +4,6 @@ class WalletTransaction < ApplicationRecord
   belongs_to :wallet
   belongs_to :order, foreign_key: :reference_id, primary_key: :id, optional: true
 
-  enum :transaction_type, { credit: 0, debit: 1, free_amount_debit: 3, free_credit: 4 }
+  enum :transaction_type, { credit: 0, debit: 1, free_amount_debit: 2, free_credit: 3 }
   enum :status, { pending: 0, success: 1, failed: 2 }
 end
