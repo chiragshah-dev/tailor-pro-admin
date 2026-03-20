d33d87714f60d7e5c207922e988ad77763544c2f
dhartiodedara27 <dharti.odedara@aspireedge.com>
2026-03-20 17:50:10 +0530

    REFINE : update migration to use separate up and down methods for dropping and recreating rolify tables

--- db/migrate/20260320063149_drop_rolify_tables.rb
+++ db/migrate/20260320063149_drop_rolify_tables.rb
@@ -1,6 +1,23 @@
 class DropRolifyTables < ActiveRecord::Migration[8.0]
-  def change
+  def up
     drop_table :admin_users_roles
     drop_table :roles
+  end
+
+  def down
+    create_table :roles do |t|
+      t.string :name
+      t.string :resource_type
+      t.bigint :resource_id
+      t.timestamps
+    end
+
+    create_table :admin_users_roles, id: false do |t|
+      t.bigint :admin_user_id
+      t.bigint :role_id
+    end
+
+    add_index :roles, [:name, :resource_type, :resource_id]
+    add_index :admin_users_roles, [:admin_user_id, :role_id]
   end
 end
